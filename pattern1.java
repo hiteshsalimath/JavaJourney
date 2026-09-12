1a92644d3ce6e28307e7c28b526aae9a742a17ae
Hitesh Salimath <139949149+hiteshsalimath@users.noreply.github.com>
2026-09-12 19:19:30 +0530

    first commit

---
+++ pattern1.java
@@ -0,0 +1,59 @@
+public class pattern1 {
+    
+    public static void main(String[] args) {
+        // PATTERN 1: sOLID SQUARE 
+        // for(int row = 1; row<=4; row++){
+        //     for(int col = 1; col<+4; col++){
+        //         System.out.print("*");
+        //     }
+        //     System.out.println();
+        // }
+        
+        //PATTERN 2:RIGHT TRIANGLE
+        // int n = 5;
+        // for(int row = 1; row<=n; row++){
+        //     for(int col = 1; col<row; col++){
+        //         System.out.print("* ");
+        //     }
+        //     System.out.println();
+        // }
+
+        //PATTERN 3: SOLID RHOMBUS:
+        
+        // for(int row = 1; row<=n; row++){
+        //     // print space
+        //     for(int col = 1; col<=n-row; col++){
+        //         System.out.print(" ");
+        //     }
+        //     // print stars
+        //     for(int col = 1;col<=n;col++){
+        //         System.out.print("* ");
+        //     }
+        //     System.out.println();
+        // }
+
+        //PATTERN 4: Inverted right Triangle:
+        // for(int row = 1; row<=n;row++){
+        //     for(int col = 1;  col <=n-row+1; col++){
+        //         System.out.print("* ");
+        //     }
+        //     System.out.println();
+        // }
+
+        //PATTERN 5: Pyramid:
+        // int n =5;
+        // for(int r = 1; r<=n; r++){
+        //     // spaces printing
+        //     for(int c = 1; c <=n-r; c++){
+        //         System.out.print("  ");
+        //     }
+        //     // star printing:
+        //     for(int c = 1; c<= (2*r)-1; c++){
+        //         System.out.print("* ");
+        //     }
+        //     System.out.println();
+        // }
+
+        
+    }
+}
